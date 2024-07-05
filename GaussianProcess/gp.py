51f0a028ef9a4b597ed70c606d38068b447c812c
PacoRahn <paco.rahn@gmail.com>
2024-07-05 17:06:14 +0200

    sample function values from the gaussian process

--- GaussianProcess/gp.py
+++ GaussianProcess/gp.py
@@ -3,11 +3,30 @@ import matplotlib.pyplot as plt
 from kernel import Kernel
 
 class GaussianProcess:
-    def __init__(self, kernel_type='rbf'):
+    def __init__(self,x,  kernel_type='rbf',mean_function = lambda x: np.zeros(x.shape[0])):
         self.kernel = Kernel(kernel_type)
+        self.covariance_matrix = self.kernel(x, x)
+        self.mean_function = mean_function
+        self.mean = self.mean_function(x) 
+        
 
 
+    def sample_functions(self, x, n_functions=1):
+        functions = np.zeros((n_functions, x.shape[0]))
+        for i in range(n_functions):
+            # Sample from the prior at our test points
+            functions[i]= self.sample_function(x)
+        return functions
 
+    def sample_function(self, x):
+        # Sample a function from the gaussian process
+        function_values = np.random.multivariate_normal(self.mean, self.covariance_matrix)
+        return function_values
+
+    def update(self, x_train, y_train):
+        # update mean and covariance matrix
+        self.mean = self.mean_function(x_train)
+        self.covariance_matrix = self.kernel(x_train, x_train)
 
     def plot_covariance_matrix(self, x):
         # Compute the covariance matrix
@@ -17,6 +36,16 @@ class GaussianProcess:
         plt.imshow(K, extent=(-3, 3, -3, 3), origin='lower', cmap='YlGnBu')
         plt.colorbar(label='k(x, x)')
         plt.title('Exponentiated quadratic example of covariance matrix')
+        plt.show()
+
+    def plot_sampled_functions(self, x, sampled_functions):
+        plt.figure(figsize=(6, 4))
+        for i in range(len(sampled_functions)):
+            plt.plot(x, sampled_functions[i], linestyle='-', marker='o', markersize=3)
+        plt.xlabel('$x$', fontsize=13)
+        plt.ylabel('$y = f(x)$', fontsize=13)
+        plt.title(f"{len(sampled_functions)} sampled functions from the GP at {len(x)} points")
+        plt.xlim([-4, 4])
         plt.show()
 
 
